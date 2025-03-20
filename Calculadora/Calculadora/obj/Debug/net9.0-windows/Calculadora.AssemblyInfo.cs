7c367256b2dbc7c18d2e8e014c74a677666c4dcb
Reverdan Almeida Sparinger <reverdan@gmail.com>
2025-03-20 09:05:16 -0300

    Calculadora
    Sem construtor

--- Calculadora/Calculadora/obj/Debug/net9.0-windows/Calculadora.AssemblyInfo.cs
+++ Calculadora/Calculadora/obj/Debug/net9.0-windows/Calculadora.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02f12ed672bd6054acaeb88cb47253aa8a1d307f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
